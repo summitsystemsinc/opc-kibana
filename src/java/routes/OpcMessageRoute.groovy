42fda49f06b09150ac421afb72b513bd37429c9b
Justin Smith <justin.smith@summitsystemsinc.com>
2014-07-28 12:24:14 -0500

    Add GEO data to the camel route.
    This can easily be added in production if the geo location is known.

--- src/java/routes/OpcMessageRoute.groovy
+++ src/java/routes/OpcMessageRoute.groovy
@@ -30,12 +30,12 @@ class OpcMessageRoute extends RouteBuilder {
 			String uriString = "opcda2:opcdaTest${group.path}?delay=${group.delay}&host=${group.host}&" +
 					"clsId=${group.clsid}" +
 					"&username=${group.user}&password=${group.password}&domain=${group.domain}&valuesOnly=false";
-
+			def type = group.indexType;
 			from(uriString).process {Exchange exchange ->
 				def newBody = [:];
 				newBody["@timestamp"] = new Date();
 				newBody["headers"] = exchange.getIn().getHeaders()
-				
+
 				exchange.getIn().getBody().each { item ->
 					def key = item.key;
 					//Get the last part of the key, on Matrikon, it is delimited by a "."
@@ -49,6 +49,38 @@ class OpcMessageRoute extends RouteBuilder {
 					}
 				}
 				exchange.getIn().setBody(newBody);
+			}.process { Exchange exchange ->
+				//Add some sample locations...
+				def body = exchange.getIn().getBody();
+				switch (group.indexType){
+					case "SawTooth":
+					//Nashville
+						body["geo"] = [
+							location:[-86.7564063, 36.186779],
+							region_name: "TN",
+							city: "Nashville"
+						]
+						break;
+					case "Triangle":
+					//Cleveland
+						body["geo"] = [
+							location:[-81.70586, 41.4949426],
+							region_name: "OH",
+							city: "Cleveland"
+						]
+						break;
+					case "Random":
+					//Chicago
+						body["geo"] = [
+							location:[-87.7321555, 41.8337329],
+							region_name: "IL",
+							city: "Chicago"
+						]
+						break;
+					default:
+						break;
+				}
+
 			}.process { Exchange exchange ->
 				exchange.getIn().setBody(exchange.getIn().getBody() as JSON)
 			}.
